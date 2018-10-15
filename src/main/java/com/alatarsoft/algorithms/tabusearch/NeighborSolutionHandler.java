accfe2f155d053cdc5bc53d8f50dfc2f9f31c8dc
Bohdan Kripak <bohdan.kripak@gmail.com>
2018-10-15 10:26:10 +0300

    Added tabusearch implementation.

---
+++ src/main/java/com/alatarsoft/algorithms/tabusearch/NeighborSolutionHandler.java
@@ -0,0 +1,27 @@
+package com.alatarsoft.algorithms.tabusearch;
+
+import java.util.List;
+
+public class NeighborSolutionHandler {
+
+    /**
+     * In every iteration we have to found the next state: so we have to consider the
+     * neighbors BUT we have to avoid states in the tabu list (so forbidden states)
+     */
+    public State getBestNeighbor(State[][] states, List<State> neighborStates, List<State> tabuStates) {
+        // it means we want to remove all items present in tabuStates from neighborStates list
+        neighborStates.removeAll(tabuStates);
+        // if all the neighboring states are in the tabu list: let's go to the middle state
+        if (neighborStates.size()==0) return states[100][100];
+        // simple linear search in O(n) to find the neighbor with smallest f(x) value
+        State bestSolution = neighborStates.get(0);
+
+        for (int i=1;i<neighborStates.size();i++)
+            if(neighborStates.get(i).getZ()<bestSolution.getZ())
+                bestSolution = neighborStates.get(i);
+
+        System.out.println("Best solution is: " + bestSolution);
+
+        return bestSolution;
+    }
+}
---
+++ src/main/java/com/alatarsoft/algorithms/tabusearch/TabuList.java
@@ -0,0 +1,29 @@
+package com.alatarsoft.algorithms.tabusearch;
+
+import org.apache.commons.collections4.IteratorUtils;
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+
+import java.util.List;
+import java.util.Queue;
+
+public class TabuList {
+
+    // this is how we represent the tabu list
+    private Queue<State> tabuList;
+
+    public TabuList() {
+        this.tabuList = new CircularFifoQueue<>(Constants.TABU_TENURE);
+    }
+
+    public void add(State solution) {
+        this.tabuList.add(solution);
+    }
+
+    public boolean contains(State solution) {
+        return this.tabuList.contains(solution);
+    }
+
+    public List<State> getTabuItems() {
+        return IteratorUtils.toList(this.tabuList.iterator());
+    }
+}
