16e710dccf510a6ea4e60379d2699786ca7ef929
bogdaneshtu <bohdan.kripak@revinate.com>
2019-01-05 21:17:54 +0200

    Added more examples.

--- src/main/java/com/alatarsoft/algorithms/hopfield/HopfieldStarter.java
+++ src/main/java/com/alatarsoft/algorithms/hopfield/HopfieldStarter.java
@@ -6,5 +6,7 @@ public class HopfieldStarter {
         HopfieldNetwork hopfieldNetwork = new HopfieldNetwork(4);
         hopfieldNetwork.train(new double[]{1,0,1,0});
         hopfieldNetwork.recall(new double[]{1,0,1,0});
+        hopfieldNetwork.recall(new double[]{0,1,0,1});
+        hopfieldNetwork.recall(new double[]{0,1,1,1});
     }
 }
